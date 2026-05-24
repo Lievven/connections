f99968abc27bf8b4072037d2aef8fbc7be44cd33
Lievven <opti8298@gmx.de>
2026-05-24 04:19:40 +0200

    Some really small half-asleep changes. Don't even know what anymore. Hope it didn't break anything.

--- phone_menu.gd
+++ phone_menu.gd
@@ -17,8 +17,7 @@ func _ready() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_home"):
-		print("Activate")
+	if event.is_action_pressed("pause"):
 		if visible:
 			deactivate_menu()
 		else:
